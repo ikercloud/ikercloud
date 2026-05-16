6b2a6bbc1fb278ed77595cbfc62a4ab5fb84b78f
ikercloud <contacto@ikercloud.com>
2026-05-16 17:31:10 -0600

    todos los archivos

---
+++ script.js
@@ -0,0 +1,18 @@
+const fadeElements = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries) => {
+
+  entries.forEach((entry) => {
+
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+
+}, {
+  threshold: 0.2
+});
+
+fadeElements.forEach((element) => {
+  observer.observe(element);
+});
